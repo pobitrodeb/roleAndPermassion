385e68f8363c18a7202a4af9d1d67db2dc750fc6
Pobitrodeb <pobitrodn@gmail.com>
2023-04-17 23:16:50 +0600

    dashboard mastering complete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\DashbaordControllerr;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/', [DashbaordControllerr::class, 'index'])->name('admin.dashboard');
+});
